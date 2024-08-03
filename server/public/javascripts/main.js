177b29d8ed15ebeb8c0b35cbf148c009175f1b18
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-08-03 20:11:03 +0800

    change to express

---
+++ server/public/javascripts/main.js
@@ -0,0 +1,89 @@
+const quoteSpace = document.getElementById("quote");
+const authorSpace = document.getElementById("author");
+const categorySpace = document.getElementById("category");
+const bwutton = document.getElementById("get");
+const disclaimer = document.getElementById("disclaimer");
+const uzonce = document.getElementById('uz');
+
+
+
+
+async function unzipGZ() {
+  if ("CompressionStream" in window) {
+    bwutton.textContent = "Unzipping data, please wait...";
+    bwutton.setAttribute("aria-busy", true);
+    bwutton.setAttribute("disabled", true);
+    disclaimer.style.visibility = "visible";
+    const compressedReadableStream = await fetch("data.gz").then(
+      (response) => response.body
+    );
+
+    const decompressedReadableStream = compressedReadableStream.pipeThrough(
+      new DecompressionStream("gzip")
+    );
+    const decomp = await new Response(decompressedReadableStream).json();
+    bwutton.textContent = "Get Random quote";
+    bwutton.setAttribute("aria-busy", false);
+    bwutton.removeAttribute("disabled");
+    disclaimer.style.visibility = "hidden";
+    return decomp;
+  } else {
+    alert("Your browser doesn't support the CompressionStream API.");
+  }
+}
+
+const decompressedData = await unzipGZ();
+
+function getRandomQuote() {
+  const stuff =
+    decompressedData[Math.floor(Math.random() * 499709)];
+  // console.log(stuff)
+  return stuff;
+}
+bwutton.onclick = function () {
+  // clear previous content
+  quoteSpace.textContent = "";
+  authorSpace.textContent = "";
+  categorySpace.textContent = "";
+
+  // make loading spinner visible
+  quoteSpace.setAttribute("aria-busy", true);
+  author.setAttribute("aria-busy", true);
+  categorySpace.setAttribute("aria-busy", true);
+
+  const content = getRandomQuote();
+
+  //set HTML contents to generated quote
+  quoteSpace.textContent = content.quote;
+  authorSpace.textContent = content.author;
+  categorySpace.textContent = content.category;
+
+  quoteSpace.setAttribute("aria-busy", false);
+  authorSpace.setAttribute("aria-busy", false);
+  categorySpace.setAttribute("aria-busy", false);
+  // });
+};
+
+// ignore below:
+// const button = document.getElementById("button");
+
+// if ("CompressionStream" in window) {
+//   button.addEventListener("click", async () => {
+//     const readableStream = await fetch("data.json").then(
+//       (response) => response.body
+//     );
+//     const compressedReadableStream = readableStream.pipeThrough(
+//       new CompressionStream("gzip")
+//     );
+//     await fileSave(new Response(compressedReadableStream), {
+//       fileName: "lorem.gz",
+//       extensions: [".gz"],
+//     });
+//   });
+// } else {
+//   button.addEventListener("click", () => {
+//     alert("Your browser doesn't support the CompressionStream API.");
+//   });
+// }
+
+// const readButton = document.getElementById("read");
---
+++ server/routes/quote.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+const fs = require("fs");
+const filePath = "./public/data/data.json";
+const inputPath = "./public/data.zip";
+const outputPath = "./public/data";
+const decompress = require("decompress");
+const getRandQuote = require("../lib/getRandQuote");
+
+router.get("/", async function (req, res, next) {
+  if (fs.existsSync(filePath)) {
+    const firstObject = await getRandQuote(filePath);
+    res.json(firstObject);
+  } else {
+    await decompress(inputPath, outputPath)
+      .then((files) => {
+        console.log("Files decompressed successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    const firstObject = await getRandQuote(filePath);
+    res.json(firstObject);
+  }
+});
+
+module.exports = router;
