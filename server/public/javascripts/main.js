fcc4117bdb1221faa2370047c317d875e039eb56
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-08-05 17:58:02 +0800

    connect frontend to backend

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -8,31 +8,31 @@ const uzonce = document.getElementById('uz');
 
 
 
-async function unzipGZ() {
-  if ("CompressionStream" in window) {
-    bwutton.textContent = "Unzipping data, please wait...";
-    bwutton.setAttribute("aria-busy", true);
-    bwutton.setAttribute("disabled", true);
-    disclaimer.style.visibility = "visible";
-    const compressedReadableStream = await fetch("data.gz").then(
-      (response) => response.body
-    );
+// async function unzipGZ() {
+//   if ("CompressionStream" in window) {
+//     bwutton.textContent = "Unzipping data, please wait...";
+//     bwutton.setAttribute("aria-busy", true);
+//     bwutton.setAttribute("disabled", true);
+//     disclaimer.style.visibility = "visible";
+//     const compressedReadableStream = await fetch("data.gz").then(
+//       (response) => response.body
+//     );
 
-    const decompressedReadableStream = compressedReadableStream.pipeThrough(
-      new DecompressionStream("gzip")
-    );
-    const decomp = await new Response(decompressedReadableStream).json();
-    bwutton.textContent = "Get Random quote";
-    bwutton.setAttribute("aria-busy", false);
-    bwutton.removeAttribute("disabled");
-    disclaimer.style.visibility = "hidden";
-    return decomp;
-  } else {
-    alert("Your browser doesn't support the CompressionStream API.");
-  }
-}
+//     const decompressedReadableStream = compressedReadableStream.pipeThrough(
+//       new DecompressionStream("gzip")
+//     );
+//     const decomp = await new Response(decompressedReadableStream).json();
+//     bwutton.textContent = "Get Random quote";
+//     bwutton.setAttribute("aria-busy", false);
+//     bwutton.removeAttribute("disabled");
+//     disclaimer.style.visibility = "hidden";
+//     return decomp;
+//   } else {
+//     alert("Your browser doesn't support the CompressionStream API.");
+//   }
+// }
 
-const decompressedData = await unzipGZ();
+// const decompressedData = await unzipGZ();
 
 function getRandomQuote() {
   const stuff =
@@ -40,27 +40,43 @@ function getRandomQuote() {
   // console.log(stuff)
   return stuff;
 }
-bwutton.onclick = function () {
+
+
+async function getData() {
+  const url = "/quote";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+bwutton.onclick = async function () {
   // clear previous content
   quoteSpace.textContent = "";
   authorSpace.textContent = "";
   categorySpace.textContent = "";
 
   // make loading spinner visible
-  quoteSpace.setAttribute("aria-busy", true);
-  author.setAttribute("aria-busy", true);
-  categorySpace.setAttribute("aria-busy", true);
+  // quoteSpace.setAttribute("aria-busy", true);
+  // author.setAttribute("aria-busy", true);
+  // categorySpace.setAttribute("aria-busy", true);
 
-  const content = getRandomQuote();
-
+  const content = await getData();
+console.log(content)
   //set HTML contents to generated quote
   quoteSpace.textContent = content.quote;
   authorSpace.textContent = content.author;
   categorySpace.textContent = content.category;
 
-  quoteSpace.setAttribute("aria-busy", false);
-  authorSpace.setAttribute("aria-busy", false);
-  categorySpace.setAttribute("aria-busy", false);
+  // quoteSpace.setAttribute("aria-busy", false);
+  // authorSpace.setAttribute("aria-busy", false);
+  // categorySpace.setAttribute("aria-busy", false);
   // });
 };
 
